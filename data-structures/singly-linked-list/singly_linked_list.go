f6c6cef6fb48343a5003036fed67d41a566e6aa4
Spencer <13088535+Spencersss@users.noreply.github.com>
2024-06-29 05:26:24 -0400

    data structures: add singly linked list (#1)
    * add initial singly linked list implementation
    
    * add pop function
    
    * add assert package and add insert at function
    
    * add delete at function
    
    * add delete tail function
    
    * add find and find at functions

---
+++ data-structures/singly-linked-list/singly_linked_list.go
@@ -0,0 +1,178 @@
+package main
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func NewNode(value int) *Node {
+	return &Node{
+		Data: value,
+		Next: nil,
+	}
+}
+
+type SinglyLinkedList struct {
+	Head *Node
+}
+
+func NewSinglyLinkedList() SinglyLinkedList {
+	return SinglyLinkedList{
+		Head: nil,
+	}
+}
+
+func (l *SinglyLinkedList) Length() int {
+	size := 0
+	curNode := l.Head
+	for curNode != nil {
+		size++
+		curNode = curNode.Next
+	}
+
+	return size
+}
+
+func (l *SinglyLinkedList) Append(value int) {
+	newNode := NewNode(value)
+
+	if l.Head == nil {
+		l.Head = newNode
+	} else {
+		curNode := l.Head
+		for curNode.Next != nil {
+			curNode = curNode.Next
+		}
+
+		curNode.Next = newNode
+	}
+}
+
+func (l *SinglyLinkedList) InsertBeforeHead(value int) {
+	newNode := NewNode(value)
+
+	if l.Head != nil {
+		newNode.Next = l.Head
+	}
+
+	l.Head = newNode
+}
+
+// InsertAt will insert a node with the provided value before the existing node at index value n.
+// If there is an error inserting the new node, it will simply be a no-op and not perform the insertion.
+func (l *SinglyLinkedList) InsertAt(value int, n int) {
+	if l.Head == nil || n == 0 {
+		l.InsertBeforeHead(value)
+	} else {
+		var lastNode *Node
+		var curNode *Node = l.Head
+		newNode := NewNode(value)
+
+		curIndex := 0
+		for curIndex < n {
+			lastNode = curNode
+			if curNode == nil {
+				return
+			}
+			curNode = curNode.Next
+			curIndex++
+		}
+
+		lastNode.Next = newNode
+		newNode.Next = curNode
+	}
+}
+
+// Pop deletes and returns the head node from the list.
+func (l *SinglyLinkedList) Pop() *Node {
+	if l.Head != nil {
+		newHead := l.Head.Next
+		l.Head.Next = nil
+		oldHead := l.Head
+		l.Head = newHead
+
+		return oldHead
+	}
+
+	return nil
+}
+
+// DeleteAt finds the existing node in the list at the provided n index and attempts to remove it from the list
+// If the node does not exist, a no-op will occur and the list will remain as is. If a node is removed, it will be returned
+// else a nil will be returned.
+func (l *SinglyLinkedList) DeleteAt(n int) *Node {
+	if l.Head != nil || n != 0 {
+		var lastNode *Node
+		var curNode *Node = l.Head
+
+		curIndex := 0
+		for curIndex < n {
+			lastNode = curNode
+			if curNode == nil {
+				return nil
+			}
+			curNode = curNode.Next
+			curIndex++
+		}
+
+		curNode.Next = nil
+		lastNode.Next = curNode.Next
+
+		return curNode
+	}
+
+	return nil
+}
+
+// DeleteTail removes the last node in the list and returns the node upon removal. If not found, the function will return nil
+func (l *SinglyLinkedList) DeleteTail() *Node {
+	if l.Head != nil {
+		var lastNode *Node
+		var curNode *Node = l.Head
+		for curNode.Next != nil {
+			lastNode = curNode
+			curNode = curNode.Next
+		}
+
+		curNode.Next = nil
+		lastNode.Next = nil
+
+		return curNode
+	}
+
+	return nil
+}
+
+// Find will search the list for the first node it comes across containing the provided data.
+// If found it will return the index of the node with the value. If not found, this will return -1.
+func (l *SinglyLinkedList) Find(value int) int {
+	curIndex := 0
+	curNode := l.Head
+	for curNode != nil {
+		if curNode.Data == value {
+			return curIndex
+		}
+
+		curIndex++
+		curNode = curNode.Next
+	}
+
+	return -1
+}
+
+// FindAt will search the list for a node at the provided n index.
+// If found it will return a pointer to the node. If not found, this will return nil.
+func (l *SinglyLinkedList) FindAt(n int) *Node {
+	curIndex := 0
+	curNode := l.Head
+	for curNode != nil {
+		if curIndex == n {
+			return curNode
+		}
+
+		curIndex++
+		curNode = curNode.Next
+	}
+
+	return nil
+}
---
+++ data-structures/singly-linked-list/singly_linked_list_test.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuccessGetLengthSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(32)
+	list.Append(6)
+
+	result := list.Length()
+	expected := 2
+
+	assert.Equalf(t, expected, result, "Expected length of expected but got %d", result)
+}
+
+func TestSuccessAppendSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(25)
+
+	expectedLength := 1
+	expectedData := 25
+
+	assert.Equalf(t, expectedLength, list.Length(), "Expected length of 1 but found %d.", list.Length())
+	assert.Equalf(t, expectedData, list.Head.Data, "Expected head node data to be value of %d but found %d.", expectedData, list.Head.Data)
+	assert.Nil(t, list.Head.Next, "Expected head next to be nil but found node.")
+}
+
+func TestSuccessInsertBeforeHeadSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(10)
+	list.InsertBeforeHead(20)
+
+	expectedLength := 2
+	expectedData := 20
+
+	assert.Equalf(t, expectedLength, list.Length(), "Expected length of %d but found %d.", expectedLength, list.Length())
+	assert.Equalf(t, expectedData, list.Head.Data, "Expected head node data to be value of 56 but found %d.", list.Head.Data)
+	assert.NotNil(t, list.Head.Next, "Expected head next to be node with value of 10.")
+}
+
+func TestSuccessInsertAtSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(11)
+	list.Append(33)
+
+	list.InsertAt(22, 1)
+	addedNode := list.Head.Next
+	expectedData := 22
+	expectedLength := 3
+
+	assert.Equalf(t, expectedLength, list.Length(), "Expected length of 3 but got %d.", list.Length())
+	assert.Equalf(t, expectedData, addedNode.Data, "Expected node value of %d but got %d.", expectedData, addedNode.Data)
+	assert.NotNil(t, addedNode.Next, "Added node should have a next node but found nil.")
+	assert.Equal(t, 33, addedNode.Next.Data, "Expected node after added node to have value of %d but found %d.", 33, addedNode.Next.Data)
+}
+
+func TestSuccessPopSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(56)
+
+	result := list.Pop()
+	expectedLength := 0
+	expectedData := 56
+
+	assert.Equalf(t, expectedLength, list.Length(), "Expected length of %d but found %d.", expectedLength, list.Length())
+	assert.Equalf(t, expectedData, result.Data, "Expected head node data to be value of 56 but found %d.", result.Data)
+}
+
+func TestSuccessDeleteAtSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(50)
+	list.Append(100)
+	list.Append(150)
+
+	result := list.DeleteAt(2)
+	expectedLength := 2
+	expectedData := 150
+
+	assert.Equalf(t, expectedLength, list.Length(), "Expected length of %d but found %d.", expectedLength, list.Length())
+	assert.Equalf(t, expectedData, result.Data, "Expected returned deleted node to have value of %d but found %d.", result.Data, expectedData)
+}
+
+func TestSuccessDeleteTailSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(300)
+	list.Append(515)
+	list.Append(902)
+
+	result := list.DeleteTail()
+	expectedLength := 2
+	expectedData := 902
+
+	assert.Equalf(t, expectedLength, list.Length(), "Expected length of %d but found %d.", expectedLength, list.Length())
+	assert.Equalf(t, expectedData, result.Data, "Expected returned deleted node to have value of %d but found %d.", result.Data, expectedData)
+}
+
+func TestSuccessFindSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(1)
+	list.Append(2)
+
+	result := list.Find(2)
+	expectedIndex := 1
+
+	assert.Equalf(t, expectedIndex, result, "Expected returned index value of %d but found %d.", expectedIndex, result)
+}
+
+func TestSuccessFindAtSinglyLinkedList(t *testing.T) {
+	list := NewSinglyLinkedList()
+	list.Append(5)
+	list.Append(4)
+	list.Append(100)
+
+	result := list.FindAt(2)
+	expectedData := 100
+
+	assert.NotNil(t, result, "Found node should not be nil but found nil.")
+	assert.Nil(t, result.Next, "Found node's next node should be nil but found node.")
+	assert.Equalf(t, expectedData, result.Data, "Expected returned node data of %d but found %d.", expectedData, result.Data)
+}
